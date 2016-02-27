53879e1979c039db2b857e2c245fd61e261d8f12
elliegarcia <emg96@uw.edu>
2016-02-26 20:16:55 -0800

    Questions 1 and 2
    Questions 1a-c should be complete

---
+++ Homework6.m
@@ -0,0 +1,24 @@
+%% 1
+load('CancerMicroarray.mat')
+%a
+length(RawMetaData.SampleName) %for number of samples
+length(X) %number of genes profiled
+%b
+[coeff, score, latent] = pca(X); %found PCA
+figure; %plotted the cumulative sum of the variance explained
+stairs(cumsum(latent)/sum(latent));
+%based on the graph looks like it's 53 PC's, I don't know how to find this
+%any other way
+%c
+%I don't understand this but it seems to work
+obj = fitcdiscr(score(:, 10), G, 'discrimtype', 'linear');
+[label, scorelin, cost] = predict(obj, score(:,10))
+%quadratic analysis
+objquad = fitcdiscr(score(:,10), G, 'discrimtype', 'quadratic');
+[labelquad, scorequad, costquad] = predict(objquad, score(:,10));
+%% 2
+load fisheriris
+fitcdiscr(meas, species)
+length(species)
+obj = fitcdiscr(meas, species, 'discrimtype', 'linear');
+[label, score, cost] = predict(obj, meas);
